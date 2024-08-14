98bcbdc7a8eadd9fc39c41a3901279d9147ea75e
ismail <ismailismo78@gmail.com>
2024-08-14 21:46:58 +0100

    chore: Add Vercel analytics to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./Theme-provider";
-
+import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -23,6 +23,7 @@ export default function RootLayout({
 					{children}
 				</ThemeProvider>
 				<SpeedInsights />
+				<Analytics />
 			</body>
 		</html>
 	);
