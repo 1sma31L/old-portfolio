8a1883d5ecd0cf7e4c1dfdcbb0c568767e7c60fd
ismail <ismailismo78@gmail.com>
2024-08-14 22:09:24 +0100

    chore: Add favicon.ico to metadata in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
 	title: "Ismail Boussekine's Protfolio",
 	description: "Modern portfolio for Ismail Boussekine",
+	icons: {
+		icon: "/favicon.ico",
+	},
 };
 
 export default function RootLayout({
