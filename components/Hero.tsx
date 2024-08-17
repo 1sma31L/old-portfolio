ed69148c89ec491f82c8e10d9cbdc56ee941ad60
ismail <ismailismo78@gmail.com>
2024-08-17 17:46:03 +0100

    perf: trying to use static text to improve performance insted TextGeneratedEffect component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import { FaLocationArrow } from "react-icons/fa";
 import MagicButton from "./ui/MagicButton";
-import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+// import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 import Link from "next/link";
 import Lights from "./ui/Lights";
 
@@ -19,10 +19,20 @@ function Hero() {
 					<p className="capitalize tracking-widest text-xs text-center dark:text-blue-100 text-black-100 max-w-80">
 						&quot;Simplicity is the soul of efficiency.&quot;
 					</p>
-					<TextGenerateEffect
+					{/* <TextGenerateEffect
 						words="Turning Ideas into Beautiful, Responsive Websites"
 						className="text-center text-[40px] md:text-4xl lg:text-4xl xl:text-5xl relative z-50"
-					/>
+					/> */}
+					<div className="font-bold text-center text-[40px] md:text-4xl lg:text-4xl xl:text-5xl relative z-50">
+						<div className="my-4">
+							<div className=" dark:text-white text-black leading-snug tracking-wide">
+								Turning Ideas into{" "}
+								<span className="text-purple">
+									Beautiful, Responsive Websites
+								</span>
+							</div>
+						</div>
+					</div>
 					<p className="text-center md:tracking-wider  text-sm md:text-lg lg:text-2xl">
 						Hi! I&apos;m
 						<strong> Ismail</strong>, a <strong>Front-End Developer</strong>{" "}
@@ -41,7 +51,7 @@ function Hero() {
 
 						lg:w-[50%] w-[75%] max-w-[450px] min-w-[270px]  
 
-				 	    rounded-full overflow-hidden hover:scale-[1.03] duration-300 transition-all shadow-xl hover:shadow-3xl border-2 dark:shadow-[0_0_60px_0_rgba(203,172,249,0.5)] border-purple">
+				 	    rounded-full overflow-hidden hover:scale-[1.03] duration-300 transition-all  hover:shadow-3xl border-2 shadow-[0_0_60px_0_rgba(0,0,255,0.2)] border-purple">
 					<img
 						src="/me.webp"
 						alt="my picture"
